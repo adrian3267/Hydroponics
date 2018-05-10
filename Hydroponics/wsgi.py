c52b07910c8c2cacf3b3e83b270a2becb1e6294d
AFZ <afz@neo.ufsc.br>
2018-05-10 12:01:17 -0300

    Update Settings

--- Hydroponics/wsgi.py
+++ Hydroponics/wsgi.py
@@ -12,7 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PPAP_Online.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Hydroponics.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
